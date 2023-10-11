121749c1c6a584a3df63d636390f219771184a34
SHIGWAN ADITYA ANANT <aditya.shigwan18@gmail.com>
2023-10-11 12:56:31 +0530

    calls and status

--- lib/src/features/chats/Chats.dart
+++ lib/src/features/chats/Chats.dart
@@ -7,23 +7,34 @@ class Chats extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return ListView(
-      children: [
-        ChatListItemWidget(
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.chat),
+      ),
+      body:  SafeArea(
+        child: ListView(
+            children: [
+            ChatListItemWidget(
             title: "Aditya",
             subTitle: "Hello there",
-            image: 'https://writestylesonline.com/wp-content/uploads/2016/08/Follow-These-Steps-for-a-Flawless-Professional-Profile-Picture-768x768.jpg',
+            image:
+            'https://writestylesonline.com/wp-content/uploads/2016/08/Follow-These-Steps-for-a-Flawless-Professional-Profile-Picture-768x768.jpg',
             lastChatTime: DateTime.now(),
             unreadCount: 2),
         ChatListItemWidget(
             title: "Anlin",
             subTitle: "Hi there",
-            image: 'https://www.epicscotland.com/wp-content/uploads/2018/01/Business-Headshot_002.jpg',
+            image:
+            'https://www.epicscotland.com/wp-content/uploads/2018/01/Business-Headshot_002.jpg',
             lastChatTime: DateTime(2023, 9, 12),
             unreadCount: 0),
+        ]
+        )
+      )
 
-      ],
+
     );
-  }
 
+  }
 }
