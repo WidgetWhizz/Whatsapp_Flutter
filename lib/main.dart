0a6c8f5443d26f2109712c51a7aedde686a33675
NunuFathima <nunufathima08360>
2023-10-11 14:32:03 +0530

    Community page completed

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class WhatsApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
- 
+
 
     return MaterialApp(
       title: 'Whatsapp',
